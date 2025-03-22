723fcf4975568ea7fa24537112d24bc26b847b48
Anubhav Goyal <anubhavgoyal@Anubhavs-MacBook-Pro.local>
2025-03-22 14:38:31 +0530

    create ticket issue

--- src/pages/CreateTicket.tsx
+++ src/pages/CreateTicket.tsx
@@ -8,7 +8,7 @@ import { Ticket, columns } from './ViewAllTicket'
 // import { columnHelper } from './ViewAllTicket'
 import { flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table'
 
-const USERID = ["A12300560079","ABC"]
+const USERID = ["A12300560079", "ABC"]
 
 // const updatedColumns = [...columns, columnHelper.accessor(row => row.conversation, {
 //     id: 'conversation',
@@ -34,15 +34,15 @@ export default function CreateTicket() {
   function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
     setForm((prev) => ({ ...prev, [e.target.name]: e.target.value }))
   }
-  async function handleSubmit(e: React.FormEvent<HTMLFormElement>){
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     try {
       setIsLoading(true);
-      await axios.post(import.meta.env.VITE_API_URL + '/create_ticket', {...form})
+      await axios.post(import.meta.env.VITE_API_URL + '/create_ticket', { ...form , user_message: form.user_message.trim()})
       window.location.reload()
     } catch (error) {
       console.error(error)
-    } finally{
+    } finally {
       setIsLoading(false);
     }
   }
@@ -59,7 +59,7 @@ export default function CreateTicket() {
         setIsLoading(false);
       }
     })()
-  },[])
+  }, [])
 
   return (
     <div className='bg-[#f7f7f7] flex h-screen w-screen'>
@@ -79,45 +79,45 @@ export default function CreateTicket() {
               <div className='flex justify-between items-center'>
                 <div className='flex flex-col'>
                   <label htmlFor="account_number" className='text-gray-500 mb-2'>Account Number</label>
-                  <select required id="account_number" name="account_number" defaultValue={USERID[0]} onChange={(e) => setForm((prev) => ({...prev,[e.target.name]: e.target.value}))}>
-                    {USERID.map((userId,index) => (<option key={index} value={userId} >{userId}</option>))}
+                  <select required id="account_number" name="account_number" defaultValue={USERID[0]} onChange={(e) => setForm((prev) => ({ ...prev, [e.target.name]: e.target.value }))}>
+                    {USERID.map((userId, index) => (<option key={index} value={userId} >{userId}</option>))}
                   </select>
                 </div>
                 <Input labelName='Heading' type="text" placeholder='Type Description Here' isRequired name='heading' handleChnage={handleInputChange} />
               </div>
               <div className='flex flex-col'>
                 <label htmlFor="user_message" className='mb-2'>Description</label>
-                <textarea required name="" id="" placeholder='Enter your query' className='border border-[#E1E4EB] rounded-md min-h-48 p-2'></textarea>
+                <textarea required name="user_message" id="user_message" placeholder='Enter your query' onChange={(e) => setForm((prev) => ({ ...prev, [e.target.name]: e.target.value }))} className='border border-[#E1E4EB] rounded-md min-h-48 p-2'></textarea>
               </div>
             </div>
           </form>
           <div className='rounded-md bg-white border border-[#E1E4EB] mt-3 p-3'>
             <div className='w-full overflow-hidden border border-[#E1E4EB] rounded-xl'>
-                  <table className='w-full'>
-                    <thead>
-                      {table.getHeaderGroups().map(headerGroup => (
-                        <tr key={headerGroup.id}>
-                          {headerGroup.headers.map(header => (
-                            <th className='p-3 text-left text-[#7B8694] border-r border-[#E1E4EB]' key={header.id}>{header.isPlaceholder ? null : flexRender(header.column.columnDef.header,header.getContext())}</th>
-                          ))}
-                        </tr>
+              <table className='w-full'>
+                <thead>
+                  {table.getHeaderGroups().map(headerGroup => (
+                    <tr key={headerGroup.id}>
+                      {headerGroup.headers.map(header => (
+                        <th className='p-3 text-left text-[#7B8694] border-r border-[#E1E4EB]' key={header.id}>{header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}</th>
                       ))}
-                    </thead>
-                    <tbody>
-                      {table.getRowModel().rows.map(row => {
-                        return (<tr key={row.id} className='border-t bg-white border-[#E1E4EB] cursor-pointer'  
-                        // onClick={() => navigate(`/ticket/${userId}/${row.getVisibleCells().filter(e => e.id.includes('ticket_id'))[0].getValue()}`)} 
-                        >
-                        {row.getVisibleCells().map(cell => (
-                          <td key={cell.id} className='p-3 border-r border-[#E1E4EB] text-[#151B23]'>
-                            {flexRender(cell.column.columnDef.cell,cell.getContext())}
-                          </td>
-                        ))}
-                      </tr>)
-                      })}
-                    </tbody>
-                  </table>
-                </div>
+                    </tr>
+                  ))}
+                </thead>
+                <tbody>
+                  {table.getRowModel().rows.map(row => {
+                    return (<tr key={row.id} className='border-t bg-white border-[#E1E4EB] cursor-pointer'
+                    // onClick={() => navigate(`/ticket/${userId}/${row.getVisibleCells().filter(e => e.id.includes('ticket_id'))[0].getValue()}`)} 
+                    >
+                      {row.getVisibleCells().map(cell => (
+                        <td key={cell.id} className='p-3 border-r border-[#E1E4EB] text-[#151B23]'>
+                          {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                        </td>
+                      ))}
+                    </tr>)
+                  })}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
